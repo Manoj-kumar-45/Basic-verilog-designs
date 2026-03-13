a89b94a8c079cd1a1f8164dc663cd854ce01b9b0
manojkumarms25 <manojkumarms462@gmail.com>
2026-03-13 22:16:46 +0530

    added tb

---
+++ dff/dff_tb.v
@@ -0,0 +1,25 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  logic d;
+  logic clk,rst;
+  logic q,qb;
+  d uuut (.d(d),.clk(clk),.rst(rst),.q(q),.qb(qb));
+  initial clk=0;
+  always #5 clk=~clk;
+  initial begin
+    rst=0;
+    d=0;
+    
+    #10 rst=1;
+    d=1;
+    #10d=0;
+    
+    #10 d=0;
+    #10 d=1;
+    #20 $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");$dumpvars;
+  end
+endmodule 
